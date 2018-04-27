1eb8038a03ab2caeefcdce115b340f8060030189
熊井誠 <sei.kumai@gmail.com>
2018-04-27 17:36:12 +0900

    メッセージ送信非同期化実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+
+$(function(){
+  function buildHTML(message){
+    var img = "";
+    if (message.image){
+      img = `<img src=${message.image} class: 'lower-message__image'>`
+    }
+    var html = `<div class="message">
+                  <ul class="upper-message">
+                    <li class="upper-message__user-name">
+                      ${message.user.name}
+                    </li>
+                    <li class="upper-message__date">
+                      ${message.created_at}
+                    </li>
+                  </ul>
+                  <ul class="lower-message">
+                      <li class="lower-message__content">
+                        ${message.content}
+                      </li>
+                        ${img}
+                  </ul>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.messages.js-messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast')
+      $('.form__submit').prop("disabled", false)
+      $('.new_message')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
