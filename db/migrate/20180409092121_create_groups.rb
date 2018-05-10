c85bcec7badb2a287618a465db46ba8dd6eae99b
熊井誠 <sei.kumai@gmail.com>
2018-05-10 21:34:17 +0900

    調整

--- db/migrate/20180409092121_create_groups.rb
+++ db/migrate/20180409092121_create_groups.rb
@@ -1,9 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     unless table_exists? :groups
-    create_table :groups do |t|
+      create_table :groups do |t|
 
       t.timestamps
+      end
     end
   end
 end
